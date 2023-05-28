1e1da83f71ec142c173be17ec3128a9977b15b74
Cattyman <88530324+cattyman919@users.noreply.github.com>
2023-05-29 00:44:44 +0700

    Added interfaces for database

--- Frontend/interfaces/index.ts
+++ Frontend/interfaces/index.ts
@@ -4,5 +4,49 @@
 //
 // import { User } from 'path/to/interfaces';
 
+export type Pelanggan = {
+  id: number
+  username : string
+  password : string
+  email : string
+  saldo : number
+}
+
+export type Restaurant = {
+  id : number
+  gambar : string
+  nama : string
+  deskripsi : Text
+  rating : number
+  review : Text
+}
+
+export type Makanan = {
+  id : number
+  gambar : string
+  nama : string
+  deskripsi : Text
+  rating : number
+  restaurant_id : number
+}
+
+export type Minuman = {
+  id : number
+  gambar : string
+  nama : string
+  deskripsi : Text
+  rating : number
+  restaurant_id : number
+}
+
+export type Order = {
+  id : number
+  restaurant_id : number
+  makanan_id : number[]
+  minuman_id : number[]
+  saldo_pelanggan : number
+  total_harga : number
+}
+
 
 
